ce0694e4369be237952741b12f320e00125f3e27
mihoko-m <mihoko@aem777.com>
2022-06-01 17:57:12 +0900

    星評価表示機能修正

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -13,15 +13,22 @@
         <h1>SJ Info.履修登録支援サイト</h1>
         <div class='reviews'>
             @foreach ($reviews as $review)
-                <div class='review'>
-                    <h2 class='title'>
+                <div class='card'>
+                    <div class="card-header">
                         <a href="/reviews/{{ $review->id }}">{{ $review->title }}</a>
-                    </h2>
-                    <div id="star">
-　                      <star-rating v-bind:star-size="20" v-bind:max-rating="5" v-bind:rating="{{ $review->get_credit }}" v-bind:read-only=true>
-　                      </star-rating>
                     </div>
-                    <p class='body'>{{ $review->body }}</p>
+                    <div class="card-body">
+                        <div id="get_credit">
+                            単位取得度
+　                          <star-rating v-bind:star-size="20" v-bind:max-rating="5" v-bind:rating="{{ $review->get_credit }}" v-bind:read-only=true>
+　                          </star-rating>
+                        </div>
+                        <div id="adequacy">
+                            充実度
+　                          <star-rating v-bind:star-size="20" v-bind:max-rating="5" v-bind:rating="{{ $review->adequacy }}" v-bind:read-only=true>
+　                          </star-rating>
+                        </div>
+                    </div>
                 </div>
             @endforeach
         </div>
